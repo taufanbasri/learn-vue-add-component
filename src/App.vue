2cd4afe8c272ab4e0a2061e97524e9524451d0f7
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-07 19:33:33 +0700

    Creating a Basic Vue App

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+        <li v-for="friend in friends" :key="friend.id">{{ friend.name }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 1,
+                    name: 'Taufan Prasetyo',
+                    phone: '123123123',
+                    email: 'taufan@mail.com'
+                },
+                {
+                    id: 2,
+                    name: 'Evita Karlina',
+                    phone: '321321321',
+                    email: 'evita@mail.com'
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
