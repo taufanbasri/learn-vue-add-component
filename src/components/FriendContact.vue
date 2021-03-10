0f4ab2074f16dea77d044a13bb39addf6fc22772
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-10 07:28:23 +0700

    More Component Communication

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -7,6 +7,7 @@
           <li><strong>Phone:</strong> {{ phoneNumber }}</li>
           <li><strong>Email:</strong> {{ emailAddress }}</li>
       </ul>
+      <button @click="$emit('delete', id)">Delete</button>
   </li>
 </template>
 
@@ -35,16 +36,17 @@ export default {
       default: false,
     },
   },
-  emits: {
-    'toggle-favorite': function (id) {
-      if (id) {
-        return true;
-      } else {
-        console.warn('ID is missing');
-        return false;
-      }
-    }
-  },
+  emits: ['toggle-favorite', 'delete'],
+  // emits: {
+  //   'toggle-favorite': function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('ID is missing');
+  //       return false;
+  //     }
+  //   }
+  // },
     data() {
         return {
             detailsAreVisible: false
